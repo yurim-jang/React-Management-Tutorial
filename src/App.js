4a6b95a30381c49efae3b76c17b56534061736af
Yurim Jang <102824372+yurim-jang@users.noreply.github.com>
2023-05-16 04:04:54 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component, component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
+const Customer = {
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'jod':'대학생',
+}
+
 class App extends Component {
 render() {
   return(
-    <Customer/>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      jod={customer.jod}
+     />
   );
 }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,10 +4,10 @@ class Customer extends React.Component {
     render() {
      return (
         <div>
-        <h2>홍길동</h2>    
-        <p>961222</p>    
-        <p>남자</p>
-        <p>대학생</p>
+        <h2>{this.props.name}</h2>    
+        <p>{this.props.birthday}</p>    
+        <p>{this.props.gender}</p>
+        <p>{this.props.job}</p>
         </div>
         )
     }
